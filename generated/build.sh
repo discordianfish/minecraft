90651e7c0068f20b89f4702716f2860f11046506
Johannes 'fish' Ziemke <github@freigeist.org>
2021-02-18 23:36:05 +0100

    Add circleci config

--- generated/build.sh
+++ generated/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-docker build -t fish/minecraft:1b8a38f -f Dockerfile .
-docker push fish/minecraft:1b8a38f
+docker build -t fish/minecraft:d15475a -f Dockerfile .
+docker push fish/minecraft:d15475a
 
