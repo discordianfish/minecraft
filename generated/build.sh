0b8be74c104f39eb31c2d8a9771bc1f7e185efa6
Johannes 'fish' Ziemke <github@freigeist.org>
2021-02-19 00:32:05 +0100

    Regenerate

--- generated/build.sh
+++ generated/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-docker build -t fish/minecraft:b8643260b28be20c3ec9a43f48d7a8c029d89cf9 -f Dockerfile .
-docker push fish/minecraft:b8643260b28be20c3ec9a43f48d7a8c029d89cf9
+docker build -t docker.io/fish/minecraft:eb01ea59d63ee34190ff2358df66b15b5036bdf5 -f Dockerfile .
+docker push docker.io/fish/minecraft:eb01ea59d63ee34190ff2358df66b15b5036bdf5
 
