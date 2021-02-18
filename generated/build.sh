a7e144d5acfc27f1feb4026b9bdd9276f2e3a664
Johannes 'fish' Ziemke <github@freigeist.org>
2021-02-19 00:07:09 +0100

    Use full git sha

--- generated/build.sh
+++ generated/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-docker build -t fish/minecraft:d15475a -f Dockerfile .
-docker push fish/minecraft:d15475a
+docker build -t fish/minecraft:b8643260b28be20c3ec9a43f48d7a8c029d89cf9 -f Dockerfile .
+docker push fish/minecraft:b8643260b28be20c3ec9a43f48d7a8c029d89cf9
 
